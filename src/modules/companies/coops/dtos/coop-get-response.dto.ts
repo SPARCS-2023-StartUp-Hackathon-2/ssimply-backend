21a924b61895b7696cec0d544bd3cd93a2354b2b
Minji Park <gbsmj0312@gmail.com>
2023-01-28 21:02:01 +0900

    fix: filedto

--- src/modules/companies/coops/dtos/coop-get-response.dto.ts
+++ src/modules/companies/coops/dtos/coop-get-response.dto.ts
@@ -1,36 +1,40 @@
 import { CoopCompany } from '@prisma/client';
 
+export class FileResponseDto {
+    name: string;
+    mimeType: string;
+    link: string;
+  
+    constructor(file: File, link: string) {
+      this.name = file.name;
+      this.mimeType = file.mimeType;
+      this.link = link;
+    }
+  }
+
+interface FileInterface {
+    type: string;
+    createdAt: Date;
+    updatedAt: Date;
+    file: FileResponseDto;
+}
+
 export class CoopGetResponseDto {
   id: number;
   name: string;
   email: string;
-  files: fileInterface[];
+  files: FileInterface[];
 
-  constructor(coop: CoopCompany) {
+  constructor(coop: CoopCompany, files: FileInterface[]) {
     this.id = coop.id;
     this.name = coop.name;
     this.email = coop.email;
-  }
-}
 
-
-interface coopCompany {
-    id: number;
-    name: string;
-    createdAt: Date;
-    updatedAt: Date;
-}
-
-export class CoopGetListResponseDto {
-  coops: coopCompany[];
-
-  constructor(coops: coopCompany[]) {
-    this.coops = coops.map((coop) =>
-      Object({ 
-        id: coop.id,
-        name: coop.name,
-        createdAt: coop.createdAt,
-        updatedAt: coop.updatedAt,
+    this.files = files.map((file) =>
+      Object({
+        name: file.name,
+        mimeType: file.mimeType,
+        link: file.link,
       }),
     );
   }
